ede8e69243c3fafbc3b3bcca457bccf7300f025d
Demetris Athienitis <56597686+athienit@users.noreply.github.com>
2023-09-27 13:19:59 -0400

    Update boostrap.R

--- boostrap.R
+++ boostrap.R
@@ -25,43 +25,56 @@ pairwise_mean_diff <- function(formula, data=NULL,indices){
     stop("The input model must be a formula.\n")
   }
   m1=aov(formula,data=dat)
-  anova.table=anova(m1)
   resp=all.vars(formula)[1]
   pred=all.vars(formula)[2]
   
   treat=as.factor(m1$model[,pred])
   r=length(levels(treat)) # number of levels
   g=r*(r-1)/2  #number of all pairwise comparisons
-  lett=combn(levels(treat),2)
+  labs=combn(levels(treat),2)
   means=tapply(dat[,resp],dat[,pred],mean)
   diffs=unname(-combn(means,2,diff)) #minus is to do A-B not B-A
   rnames=rep(0,g)
   for(i in 1:g){
-    rnames[i]=paste(lett[1,i],"-",lett[2,i])
+    rnames[i]=paste(labs[1,i],"-",labs[2,i])
   }
-  setNames(diffs,rnames)
+  diffs=setNames(diffs,rnames)
   return(diffs)
 }
-
 
 # Perform bootstrap
 bootstrap_results <- boot(data=mydata,statistic=pairwise_mean_diff, sim="ordinary",R = 1000,formula=value~group)
 # use index=1 for 1st parameter (difference), 2 for 2nd etc
-generate_results <- function(results) {
+generate_results <- function(results,conf.level=0.95,bonf.adj=T) {
+  #Create labels
+  m1=aov(formula,data=dat)
+  resp=all.vars(formula)[1]
+  pred=all.vars(formula)[2]
+  treat=as.factor(m1$model[,pred])
+  r=length(levels(treat)) # number of levels
+  g=r*(r-1)/2  #number of all pairwise comparisons
+  if(bonf.adj==T){conf=round(1-(1-conf.level)/g,3)} #bonferroni adjustment
+  labs=combn(levels(treat),2)
+  rnames=rep(0,g)
+  for(i in 1:g){
+    rnames[i]=paste(labs[1,i],"-",labs[2,i])
+  }
+  
   # Create a loop to generate plots
   num_plots=dim(results$t0)
   for (i in 1:num_plots) {
     # Create a new plot
     plot(results,index=i)
-    title(paste("Plot",i))
+    title(paste("  |  Plot:",rnames[i]))
   }
   # Print the results
-  cat("Bootstrap results:\n")
+  cat("Bootstrap results for differences:\n")
+  cat(rnames,sep ="\n")
   print(bootstrap_results)
   for (i in 1:num_plots) {
     cat("\n")
-    print(paste("95% Confidence Intervals for Pairwise Mean Differences:",i))
-    print(boot.ci(boot.out=bootstrap_results, type="all",index=i)) # BCa preferred
+    print(paste("CI for Pairwise Mean Differences:",rnames[i]))
+    print(boot.ci(boot.out=bootstrap_results, type="all",index=i,conf=conf)) # BCa preferred
   }
 }
 
